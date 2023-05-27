feb5ea8084365176e3af3fe482d7ff2e61215c33
BiancaLopresto <bianlopre19@gmail.com>
2023-05-27 00:51:02 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio20b.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio20b.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+
+            Console.Write("Ingres el primer numero: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            numero2 = double.Parse(Console.ReadLine());
+
+            if (numero1 == numero2)
+            {
+                Console.WriteLine("Ambos numeros son iguales");
+            }
+            else
+            {
+                if (numero1 > numero2)
+                {
+                    Console.Write($"{numero1} mayor que {numero2}");
+                }
+                else
+                {
+                    Console.Write($"{numero2} mayor que {numero1}");
+                }
+            }
+        }
+    }
+}
